8772c43d8e9a4240dc51760e7d2e817e4202feee
jafar <aljuneidi.jafar@gmail.com>
2022-11-28 15:57:25 +0200

    version 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,81 @@
-use std::{net::IpAddr, str::FromStr};
+use std::io::{self, Write};
+use std::net::{IpAddr, Ipv4Addr};
+use std::sync::mpsc;
+use tokio::net::TcpStream;
+use tokio::task;
 
+use bpaf::Bpaf;
+
+const IPFALLBACK: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+const MAX: u16 = 65535;
+
+#[derive(Debug, Clone, Bpaf)]
+#[bpaf(options)]
 pub struct Config {
-    pub flag: String,
-    pub ipaddr: IpAddr,
-    pub threads: u16,
+    #[bpaf(long, short, fallback(IPFALLBACK))]
+    /// The address that you want to sniff. Must be a valid Ipv4 address. Falls back to 127.0.0.1
+    pub address: IpAddr,
+
+    #[bpaf(
+        long("start"),
+        short('s'),
+        fallback(1u16),
+        guard(start_port_guard, "Must be greater than 0")
+    )]
+    /// The start port for the sniffer. (must be greater than 0)
+    pub start_port: u16,
+
+    #[bpaf(
+        long("end"),
+        short('e'),
+        fallback(MAX),
+        guard(end_port_guard, "Must be less than or equal to 65535")
+    )]
+    /// The end port for the sniffer. (must be less than or equal to 65535)
+    pub end_port: u16,
 }
 
-impl Config {
-    pub fn build<T: Iterator<Item = String>>(args: T) -> Result<Config, &'static str> {
-        let args: Vec<String> = args.collect();
+fn start_port_guard(input: &u16) -> bool {
+    *input > 0
+}
 
-        if args.len() < 2 {
-            return Err("Not enough arguments");
-        } else if args.len() > 4 {
-            return Err("Too mant arguments");
+fn end_port_guard(input: &u16) -> bool {
+    *input <= MAX
+}
+
+async fn scan(tx: mpsc::Sender<u16>, port: u16, addr: IpAddr) {
+    match TcpStream::connect(format!("{}:{}", addr, port)).await {
+        Ok(_) => {
+            print!(".");
+            io::stdout().flush().unwrap();
+            tx.send(port).unwrap();
         }
+        Err(_) => {}
+    }
+}
 
-        let address = args[1].clone();
-        if let Ok(ipaddr) = IpAddr::from_str(&address) {
-            return Ok(Config {
-                flag: String::from(""),
-                ipaddr,
-                threads: 4,
-            });
-        }
+#[tokio::main]
+pub async fn run() {
+    let opts: Config = config().run();
 
-        let flag = address;
-        if flag.contains("-h") || flag.contains("-help") {
-            if args.len() == 2 {
-                return Err("help");
-            } else {
-                return Err("To many arguments");
-            }
-        }
+    let (tx, rx) = mpsc::channel();
+    for i in opts.start_port..opts.end_port {
+        let tx = tx.clone();
 
-        if flag.contains("-t") {
-            let ipaddr = match IpAddr::from_str(&args[3]) {
-                Ok(x) => x,
-                Err(_) => return Err("Not valid IP address, my be IPV4 or IPV6"),
-            };
+        task::spawn(async move {
+            scan(tx, i, opts.address).await;
+        });
+    }
 
-            let threads = match args[2].parse::<u16>() {
-                Ok(num) => num,
-                Err(_) => return Err("Failed to parse thread number"),
-            };
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
 
-            return Ok(Config {
-                flag,
-                ipaddr,
-                threads,
-            });
-        }
-
-        return Err("Invalid Syntax");
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
     }
 }
